623a2e80acb33726079eea80d63498f608099357
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-11-17 12:43:37 +0100

    Minimal update to tests.

--- tests/test_dafsa.py
+++ tests/test_dafsa.py
@@ -13,6 +13,7 @@ import unittest
 
 # Import the library itself
 import dafsa
+import dafsa.utils
 
 
 class TestDAFSA(unittest.TestCase):
@@ -25,8 +26,7 @@ class TestDAFSA(unittest.TestCase):
         seqs = ["tap","taps","top","tops"]
 
         # build object
-        dafsa_obj = dafsa.DAFSA()
-        dafsa_obj.insert(seqs)
+        dafsa_obj = dafsa.DAFSA(seqs)
 
 
     def test_full_test(self):
@@ -35,18 +35,17 @@ class TestDAFSA(unittest.TestCase):
         """
 
         # Load strings from file
-        filename = dafsa.RESOURCE_DIR / "ciura.txt"
+        filename = dafsa.utils.RESOURCE_DIR / "ciura.txt"
         with open(filename.as_posix()) as handler:
             strings = [line.strip() for line in handler]
 
         # build object
-        dafsa_obj = dafsa.DAFSA()
-        dafsa_obj.insert(strings)
+        dafsa_obj = dafsa.DAFSA(strings)
 
         # don't print
         text = str(dafsa_obj)
 
         # simple checks
-        assert dafsa_obj.lookup("deny") is not False
-        assert dafsa_obj.lookup("dafsa") is False
-        assert dafsa_obj.lookup("dawg") is False
+        assert dafsa_obj.lookup("deny") is not None
+        assert dafsa_obj.lookup("dafsa") is None
+        assert dafsa_obj.lookup("dawg") is None
