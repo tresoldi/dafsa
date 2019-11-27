23f14b7dffa99bdde438a17981e03a8ee2465787
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-11-27 22:12:51 +0100

    Redoing tests

--- tests/test_dafsa.py
+++ tests/test_dafsa.py
@@ -37,29 +37,45 @@ class TestNode(unittest.TestCase):
         node_d.edges["x"] = dafsa.dafsa.DAFSAEdge(node_b, 1)
 
         # __str__ and __repr__ assertions
-        assert str(node_a) == ""
-        assert str(node_b) == ""
-        assert str(node_c) == "x|1"
-        assert str(node_d) == "x|1"
+        if not str(node_a) == "":
+            raise AssertionError()
+        if not str(node_b) == "":
+            raise AssertionError()
+        if not str(node_c) == "x|1":
+            raise AssertionError()
+        if not str(node_d) == "x|1":
+            raise AssertionError()
 
-        assert repr(node_a) == "0()"
-        assert repr(node_b) == "F()"
-        assert repr(node_c) == "n(#1:<x>/2)"
-        assert repr(node_d) == "n(#1:<x>/1)"
+        if not repr(node_a) == "0()":
+            raise AssertionError
+        if not repr(node_b) == "F()":
+            raise AssertionError
+        if not repr(node_c) == "n(#1:<x>/2)":
+            raise AssertionError
+        if not repr(node_d) == "n(#1:<x>/1)":
+            raise AssertionError
 
         # __eq__ assertions
-        assert node_a == node_b
-        assert node_c == node_d
-        assert node_a != node_c
+        if not node_a == node_b:
+            raise AssertionError
+        if not node_c == node_d:
+            raise AssertionError
+        if not node_a != node_c:
+            raise AssertionError
 
         # __gt__ assertions
-        assert node_a < node_c
-        assert node_d > node_b
+        if not node_a < node_c:
+            raise AssertionError
+        if not node_d > node_b:
+            raise AssertionError
 
         # __hash__ assertions, follow _str__ for now
-        assert hash(node_a) == hash(node_b)
-        assert hash(node_c) == hash(node_d)
-        assert hash(node_a) != hash(node_c)
+        if not hash(node_a) == hash(node_b):
+            raise AssertionError
+        if not hash(node_c) == hash(node_d):
+            raise AssertionError
+        if not hash(node_a) != hash(node_c):
+            raise AssertionError
 
 
 class TestEdge(unittest.TestCase):
@@ -83,9 +99,12 @@ class TestEdge(unittest.TestCase):
         edge_c = dafsa.dafsa.DAFSAEdge(node_b)
 
         # __str__ assertions
-        assert str(edge_a) == "{node_id: 15, weight: 1}"
-        assert str(edge_b) == "{node_id: 15, weight: 2}"
-        assert str(edge_c) == "{node_id: 16, weight: 1}"
+        if not str(edge_a) == "{node_id: 15, weight: 0}":
+            raise AssertionError
+        if not str(edge_b) == "{node_id: 15, weight: 2}":
+            raise AssertionError
+        if not str(edge_c) == "{node_id: 16, weight: 0}":
+            raise AssertionError
 
 
 class TestDAFSA(unittest.TestCase):
@@ -97,6 +116,7 @@ class TestDAFSA(unittest.TestCase):
         seqs = ["tap", "taps", "top", "tops"]
 
         # build object
+        # TODO: write test?
         dafsa_obj = dafsa.DAFSA(seqs)
 
     def test_full_test(self):
@@ -116,10 +136,14 @@ class TestDAFSA(unittest.TestCase):
         text = str(dafsa_obj)
 
         # simple checks
-        assert dafsa_obj.lookup("den") is None
-        assert dafsa_obj.lookup("deny") is not None
-        assert dafsa_obj.lookup("dafsa") is None
-        assert dafsa_obj.lookup("dawg") is None
+        if not dafsa_obj.lookup("den") is None:
+            raise AssertionError
+        if not dafsa_obj.lookup("deny") is not None:
+            raise AssertionError
+        if not dafsa_obj.lookup("dafsa") is None:
+            raise AssertionError
+        if not dafsa_obj.lookup("dawg") is None:
+            raise AssertionError
 
 if __name__ == "__main__":
     sys.exit(unittest.main())
